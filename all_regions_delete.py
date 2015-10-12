1012161ebef577f73f94b84657d2ea52a101eba8
Tomas Nunez <nunez@pythian.com>
2015-10-12 14:11:28 +0200

    test

--- all_regions_delete.py
+++ all_regions_delete.py
@@ -6,6 +6,7 @@ regions = ['us-east-1','us-west-1','us-west-2','eu-west-1','sa-east-1',
             'ap-southeast-1','ap-southeast-2','ap-northeast-1']
 #regions = ['ap-southeast-1']
 
+
 for region in regions:
     print ("Region " + region)
     instances = vpc_con.get_only_instances(filters=({"instance-state-name": [ "pending", "running", "stopping", "stopped", "shutting-down" ]}))
